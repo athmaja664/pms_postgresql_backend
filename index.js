da68c819ed48e296699d6b514b4b8539a73d7874
athmaja664 <athmajavijayakumar@gmail.com>
2026-06-17 14:43:48 +0530

    index file updated

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const express = require('express')
 const db = require('./config/db')
 const cors = require('cors')
-const router=require('./router/routes')
+const router = require('./router/routes')
+
+console.log('CLOUD_NAME:', process.env.CLOUDINARY_CLOUD_NAME)
+console.log('API_KEY:', process.env.CLOUDINARY_API_KEY)
+console.log('API_SECRET length:', process.env.CLOUDINARY_API_SECRET ? process.env.CLOUDINARY_API_SECRET.length : 'MISSING')
+
 const pmsServer = express()
 pmsServer.use(cors())
 pmsServer.use(express.json())
@@ -10,5 +15,4 @@ pmsServer.use('/uploads', express.static('uploads'))
 const PORT = 3000
 pmsServer.listen(PORT, () => {
     console.log(`pms server start on port ${PORT}`);
-
 })
