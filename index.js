37ecc15f1d3896c7bcde5d7bed5e1ed154833035
athmaja664 <athmajavijayakumar@gmail.com>
2026-06-18 11:58:32 +0530

    desihn updated

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ const db = require('./config/db')
 const cors = require('cors')
 const router = require('./router/routes')
 
-console.log('CLOUD_NAME:', process.env.CLOUDINARY_CLOUD_NAME)
-console.log('API_KEY:', process.env.CLOUDINARY_API_KEY)
-console.log('API_SECRET length:', process.env.CLOUDINARY_API_SECRET ? process.env.CLOUDINARY_API_SECRET.length : 'MISSING')
+// console.log('CLOUD_NAME:', process.env.CLOUDINARY_CLOUD_NAME)
+// console.log('API_KEY:', process.env.CLOUDINARY_API_KEY)
+// console.log('API_SECRET length:', process.env.CLOUDINARY_API_SECRET ? process.env.CLOUDINARY_API_SECRET.length : 'MISSING')
 
 const pmsServer = express()
 pmsServer.use(cors())
