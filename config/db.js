5af77dfd2c2c0330947d72cb156dee08a19a213a
athmaja664 <athmajavijayakumar@gmail.com>
2026-06-29 13:01:11 +0530

    updated

--- config/db.js
+++ config/db.js
@@ -1,16 +1,19 @@
 require('dotenv').config()
 const { Client } = require('pg')
-const con=new Client({
-    host:process.env.DB_HOST,
-    user:process.env.DB_USER,
-    port:process.env.DB_PORT,
-    password:process.env.DB_PASSWORD,
-    database:process.env.DB_DATABASE
+
+const con = new Client({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    port: process.env.DB_PORT,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    ssl: {
+        rejectUnauthorized: false
+    }
 })
 
-con.connect().then(()=>{
-    console.log("connected");
-    
-}).catch((err)=>{console.log(err);
-})
-module.exports=con
+con.connect()
+.then(() => console.log("connected"))
+.catch(err => console.log(err))
+
+module.exports = con
