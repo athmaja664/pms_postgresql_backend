ad5f3a23ba983aaccbd3b391582e7504b136f609
athmaja664 <athmajavijayakumar@gmail.com>
2026-07-17 12:08:02 +0530

    Added forgot and reset passwrd

--- config/db.js
+++ config/db.js
@@ -1,36 +1,36 @@
-require('dotenv').config()
-const { Client } = require('pg')
-
-const con = new Client({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    port: process.env.DB_PORT,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
-    ssl: {
-        rejectUnauthorized: false
-    }
-})
-
-con.connect()
-.then(() => console.log("connected"))
-.catch(err => console.log(err))
-
-module.exports = con
-
 // require('dotenv').config()
 // const { Client } = require('pg')
-// const con=new Client({
-//     host:process.env.DB_HOST,
-//     user:process.env.DB_USER,
-//     port:process.env.DB_PORT,
-//     password:process.env.DB_PASSWORD,
-//     database:process.env.DB_DATABASE
+
+// const con = new Client({
+//     host: process.env.DB_HOST,
+//     user: process.env.DB_USER,
+//     port: process.env.DB_PORT,
+//     password: process.env.DB_PASSWORD,
+//     database: process.env.DB_DATABASE,
+//     ssl: {
+//         rejectUnauthorized: false
+//     }
 // })
 
-// con.connect().then(()=>{
-//     console.log("connected");
+// con.connect()
+// .then(() => console.log("connected"))
+// .catch(err => console.log(err))
+
+// module.exports = con
+
+require('dotenv').config()
+const { Client } = require('pg')
+const con=new Client({
+    host:process.env.DB_HOST,
+    user:process.env.DB_USER,
+    port:process.env.DB_PORT,
+    password:process.env.DB_PASSWORD,
+    database:process.env.DB_DATABASE
+})
+
+con.connect().then(()=>{
+    console.log("connected");
     
-// }).catch((err)=>{console.log(err);
-// })
-// module.exports=con
+}).catch((err)=>{console.log(err);
+})
+module.exports=con
