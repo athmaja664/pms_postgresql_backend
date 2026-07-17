525ec23f88be5ec725b6a7a40eb741f32559580e
athmaja664 <athmajavijayakumar@gmail.com>
2026-07-17 16:48:03 +0530

    Added mailer verify

--- config/mailer.js
+++ config/mailer.js
@@ -1,21 +1,21 @@
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com",
-    port: 587,
-    secure: false,
-    auth: {
-        user: process.env.EMAIL,
-        pass: process.env.EMAIL_PASSWORD,
-    },
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
+  auth: {
+    user: process.env.EMAIL,
+    pass: process.env.EMAIL_PASSWORD,
+  },
 });
 
 transporter.verify((error, success) => {
-    if (error) {
-        console.log("Mailer Error:", error);
-    } else {
-        console.log("Mailer is ready");
-    }
+  if (error) {
+    console.log("Mailer Error:", error);
+  } else {
+    console.log("Mailer is ready");
+  }
 });
 
 module.exports = transporter;
